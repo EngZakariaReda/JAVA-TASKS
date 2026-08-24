15bb69c86e20f870c50d7e2dfd91a63d5d205d1b
engzeko <mzkryarda@gmail.com>
2026-08-24 22:18:16 +0300

    finishing all task

--- Level 1 basics/4 Exception Handling/Task/Bonus _Challenging Tasks/Finally Block Demo/src/Main.java
+++ Level 1 basics/4 Exception Handling/Task/Bonus _Challenging Tasks/Finally Block Demo/src/Main.java
@@ -1,13 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("please enter number");
+        Scanner sc = new Scanner(System.in);
+        try {
+
+            int number = sc.nextInt();
+            System.out.println("your number is " + number);
+
+            System.out.println(10 / 0);
+        } catch (InputMismatchException e) {
+            System.out.println("please enter number only");
+        } finally {
+            System.out.println("Finally block executed even if there is exception not handled");
+        }
     }
 }
